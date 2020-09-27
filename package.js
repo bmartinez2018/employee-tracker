60853e14cb80b47a741e91f8d014c046be526f85
bmartinez2018 <67445858+bmartinez2018@users.noreply.github.com>
2020-09-26 21:16:01 -0500

    Add files via upload

---
+++ package.js
@@ -0,0 +1,25 @@
+
+{
+    "name": "tracker",
+    "version": "1.0.0",
+    "description": "CLI employee CMS",
+    "main": "app.js",
+    "scripts": {
+      "test": "echo \"Error: no test specified\" && exit 1"
+    },
+    "repository": {
+      "type": "git",
+      "url": ""
+    },
+    "author": "Bmartinez2018",
+    "license": "MIT",
+    "bugs": {
+      "url": ""
+    },
+    "homepage": "",
+    "dependencies": {
+      "console.table": "^0.10.0",
+      "inquirer": "^7.0.0",
+      "mysql": "^2.17.1"
+    }
+  }
